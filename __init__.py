7b46a1cc93ad559ad3d481acf636aa165354b82b
Er1ckR1ck0 <eryckov37@gmail.com>
2026-04-23 02:54:02 +0300

    Add LockState and DeviceType to module exports in __init__.py

--- __init__.py
+++ __init__.py
@@ -1,14 +1,17 @@
-from ttlock_modules.client import LockClient
-from ttlock_modules.constants import LockProvider, ENDPOINTS
-from ttlock_modules.enums import PasscodeType
-from ttlock_modules.exceptions import LockAPIError
-from ttlock_modules.providers import TTLockClient
+from client import LockClient
+from modules.constants import LockProvider, ENDPOINTS
+from enums import PasscodeType, LockState, DeviceType
+from modules.exceptions import LockAPIError
+from providers import TTLockClient
 
 __all__ = [
     "LockClient",
     "TTLockClient",
     "LockProvider",
     "PasscodeType",
+    "LockState",
+    "DeviceType",
     "ENDPOINTS",
     "LockAPIError",
 ]
+
